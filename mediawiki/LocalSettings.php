0cdf8e309eec3f38521f10cfe929693167262dcd
Ross Rosen <rrosen326@gmail.com>
2023-03-08 14:25:54 -0800

    Successfully built mediawiki with chameleon skin. Vanilla chameleon install

--- mediawiki/LocalSettings.php
+++ mediawiki/LocalSettings.php
@@ -126,7 +126,7 @@ $wgGroupPermissions['*']['edit'] = false;
 
 ## Default skin: you can change the default skin. Use the internal symbolic
 ## names, e.g. 'vector' or 'monobook':
-$wgDefaultSkin = "vector";
+# $wgDefaultSkin = "vector";
 
 # Enabled skins.
 # The following skins were automatically enabled:
@@ -149,3 +149,7 @@ wfLoadExtension( 'WikiEditor' );
 # End of automatically generated settings.
 # Add more configuration options below.
 
+# Chameleon skin: https://github.com/ProfessionalWiki/chameleon/blob/master/docs/installation.md
+wfLoadExtension( 'Bootstrap' );
+wfLoadSkin( 'chameleon' );
+$wgDefaultSkin='chameleon';
