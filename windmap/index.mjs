557fe0522ad03c42a71189c920638fbd9d238fc8
Ross Rosen <rrosen326@gmail.com>
2024-04-08 13:51:49 -0700

    Moving from .js to .mjs (with import, etc)
    Not exactly sure why I had to do this but something
    wasn't working and this fixed it.

---
+++ windmap/index.mjs
@@ -0,0 +1,147 @@
+
+
+import puppeteer  from "puppeteer";
+import cheerio from "cheerio";
+import { LocalStorage } from "node-localstorage";
+
+if (typeof localStorage === "undefined" || localStorage === null) {
+    var localStorage = new LocalStorage("./windmap/tmp");
+
+}
+
+async function fetchPageRaw(pageUrl) {
+    let response = null,
+        body = null;
+    console.log("fetching", pageUrl);
+    try {
+        response = await fetch(pageUrl);
+        if (response.ok) {
+            console.log("got page response", pageUrl);
+            body = await response.text();
+            console.log("got body", pageUrl);
+        } else {
+            throw new Error("Network response was not ok.");
+        }
+    } catch (error) {
+        console.error("Error fetching", error);
+    } finally {
+        return body;
+    }
+}
+
+async function fetchPageJS(pageUrl) {
+    if (requiresLogin(pageUrl)) {
+        await loginSite(pageUrl);
+    }
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto(pageUrl, { waitUntil: "networkidle0" });
+    await new Promise((r) => setTimeout(r, 1000)); // wait for JS to load. 1 s enough?
+    let html = await page.content();
+    return html;
+}
+
+async function getPage(pageUrl, useCache = false) {
+    console.log("getpage", pageUrl);
+    let page = localStorage.getItem(pageUrl);
+    if (useCache && page) {
+        console.log("got page from local storage", pageUrl);
+    } else {
+        page = await fetchPageJS(pageUrl);
+        localStorage.setItem(pageUrl, page);
+        console.log("got page from fetch and saved to local storage", pageUrl);
+    }
+    return page;
+}
+
+function tempestParsePage(html) {
+    const $ = cheerio.load(html);
+    let ccWind = $("#cc-wind");
+    let windDirection = ccWind
+        .find(".wind-direction-icon")
+        .attr("data-wind-direction");
+    let windAvg = ccWind.find("span[data-param=wind_avg]").text();
+    let pageTimestamp = $("#list-summary-view")
+        .find("span[data-param=timestamp]")
+        .text();
+    let rapidWindTimestamp = ccWind.attr("data-rapid_wind_timestamp");
+    return {
+        windDirection: windDirection,
+        windAvg: windAvg,
+        pageTimestamp: pageTimestamp,
+        rapidWindTimestamp: rapidWindTimestamp,
+    };
+}
+
+function iWindsurfParsePage(html) {
+    const $ = cheerio.load(html);
+    let cc = $("#current-conditions .spot-info-container");
+    if (cc.length == 0) {
+        throw new Error("No current conditions found - must be logged in!");
+    }
+    return {
+        windDirection: windDirection,
+        windAvg: windAvg,
+        pageTimestamp: pageTimestamp,
+        rapidWindTimestamp: rapidWindTimestamp,
+    };
+}
+
+async function loginiWindsurf() {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto("https://secure.iwindsurf.com/", {
+        waitUntil: "networkidle0",
+    });
+    let html = await page.content();
+    return html;
+}
+
+async function loginSite(url) {
+    if (url.startsWith("https://wx.iwindsurf.com")) {
+        return loginiWindsurf();
+    } else {
+        throw new Error("Unknown URL to login: " + url);
+    }
+}
+
+function requiresLogin(url) {
+    return (
+        url.startsWith("https://wx.ikitesurf.com") ||
+        url.startsWith("https://wx.iwindsurf.com")
+    );
+}
+
+async function parseHtml(html, url) {
+    if (url.startsWith("https://tempestwx.com")) {
+        return tempestParsePage(html);
+    } else if (
+        url.startsWith("https://wx.ikitesurf.com") ||
+        url.startsWith("https://wx.iwindsurf.com")
+    ) {
+        return iWindsurfParsePage(html);
+    } else {
+        throw new Error("Unknown URL to parse: " + url);
+    }
+}
+
+async function main(locations) {
+    for await (const location of locations) {
+        let html = await getPage(location.url, false);
+        let windData = parseHtml(html, location.url);
+        console.log("Back from getPage: ", location.name, windData);
+    }
+}
+
+let locations = [
+    //{name: 'Waverlyish', url: 'https://tempestwx.com/station/105376/'},
+    {
+        name: "Golden Gardens Light 2",
+        url: "https://wx.iwindsurf.com/spot/93975",
+    },
+];
+
+// main()
+console.log("windmap/index.js running!");
+await main(locations);
+console.log("windmap/index.js done!");
