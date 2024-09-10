c8b4baec81301c1aa5748d9815e73e60755143e7
Ross Rosen <rrosen326@gmail.com>
2024-09-10 15:39:05 -0700

    tweaks

--- tasks.py
+++ tasks.py
@@ -52,36 +52,7 @@ def regenerate(c):
 
 @task
 def serve(c):
-    """Serve site at http://$HOST:$PORT/ (default is localhost:8000)"""
-
-    class AddressReuseTCPServer(RootedHTTPServer):
-        allow_reuse_address = True
-
-    server = AddressReuseTCPServer(
-        CONFIG["deploy_path"],
-        (CONFIG["host"], CONFIG["port"]),
-        ComplexHTTPRequestHandler,
-    )
-
-    if OPEN_BROWSER_ON_SERVE:
-        # Open site in default browser
-        import webbrowser
-
-        webbrowser.open("http://{host}:{port}".format(**CONFIG))
-
-    sys.stderr.write("Serving at {host}:{port} ...\n".format(**CONFIG))
-    server.serve_forever()
-
-
-@task
-def preview(c):
-    """Build production version of site"""
-    pelican_run("-s {settings_publish}".format(**CONFIG))
-
-
-@task
-def livereload(c):
-    """Automatically reload browser tab upon file modification."""
+    """Serve a development version of the site"""
     from livereload import Server
 
     def cached_build():
